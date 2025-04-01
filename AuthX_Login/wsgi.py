b55f34f19268e5836ad6d4e993ac198655ab7093
Diljithm4 <Diljithm4@gmail.com>
2025-04-01 16:36:02 +0530

    initial commit

---
+++ AuthX_Login/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AuthX_Login project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AuthX_Login.settings')
+
+application = get_wsgi_application()
